bf83fb958ace639062ca33e0772930093a1d71d8
patora58 <patora58@gmail.com>
2021-03-09 21:22:21 +0100

    Hello Java

---
+++ hello/src/main/java/patorka/hello/Program.java
@@ -0,0 +1,17 @@
+
+package patorka.hello;
+
+/**
+ *
+ * @author mariu
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello***");
+    }
+    
+}
